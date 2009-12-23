23db38e9ad8702e5d48360d17dc0ef0b50bad531
Ingo Breßler <libcfp@ingobressler.net>
2009-12-23 01:23:21 +0000

    - removed obsolete debug output from test_auto_parser

--- tests/test_auto_parser.cpp
+++ tests/test_auto_parser.cpp
@@ -31,7 +31,6 @@
 
 TEST(ParserConstructor)
 {
-	std::cerr << "TEST(ParserConstructor) start" << std::endl;
 	cfp::Parser p;
 	CHECK(p.formula().empty());
 	CHECK(p.empirical().empty());
@@ -42,12 +41,10 @@ TEST(ParserConstructor)
 	cfp::Parser p1("foo", 3);
 	CHECK_EQUAL(0, p1.formula().compare("foo"));
 	CHECK_EQUAL((size_t)3, p1.formula().length());
-	std::cerr << "TEST(ParserConstructor) stop" << std::endl;
 }
 
 TEST(ParserGetterSetterCopyConstructor)
 {
-	std::cerr << "TEST(ParserGetterSetterCopyConstructor) start" << std::endl;
 	cfp::Parser p;
 	p.setFormula("foobar", 6);
 	CHECK_EQUAL(0, p.formula().compare("foobar"));
@@ -64,75 +61,61 @@ TEST(ParserGetterSetterCopyConstructor)
 	CHECK_EQUAL((size_t)3, p1.formula().length());
 	CHECK_EQUAL((size_t)46, p1.maxNestingLevel());
 	CHECK(p1.empirical().empty());
-	std::cerr << "TEST(ParserGetterSetterCopyConstructor) stop" << std::endl;
 }
 
 #define TEST_FORMULA_1 "K3 (4J)2.3 (13C)1.2"
 TEST(ParserProcessingRegular1)
 {
-	std::cerr << "TEST(ParserProcessingRegular1) start" << std::endl;
 	cfp::Parser p("K3(J(4))2.3(13C)1.2",19);
 	p.process();
-	std::cerr << "TEST(ParserProcessingRegular1), process done" << std::endl;
 	CHECK_EQUAL((size_t)3, p.empirical().size());
-	std::cerr << "TEST(ParserProcessingRegular1), empirical1" << std::endl;
 	std::stringstream ss;
 	ss << p.empirical();
-	std::cerr << "TEST(ParserProcessingRegular1), empirical2" << std::endl;
 	CHECK_EQUAL(0, ss.str().compare("(13C)1.2 (4J)2.3 K3"));
-	std::cerr << "TEST(ParserProcessingRegular1) stop" << std::endl;
 }
 
 #define TEST_FORMULA_2 "H2.3O"
 TEST(ParserProcessingRegular2)
 {
-	std::cerr << "TEST(ParserProcessingRegular2) start" << std::endl;
 	cfp::Parser p(TEST_FORMULA_2, sizeof(TEST_FORMULA_2)-1);
 	p.process();
 	CHECK_EQUAL((size_t)2, p.empirical().size());
 	std::stringstream ss;
 	ss << p.empirical();
 	CHECK_EQUAL(0, ss.str().compare("H2.3 O"));
-	std::cerr << "TEST(ParserProcessingRegular2) stop" << std::endl;
 }
 
 #define TEST_FORMULA_3 "H2,3O"
 TEST(ParserProcessingRegular3)
 {
-	std::cerr << "TEST(ParserProcessingRegular3) start" << std::endl;
 	cfp::Parser p(TEST_FORMULA_3, sizeof(TEST_FORMULA_3)-1);
 	p.process();
 	CHECK_EQUAL((size_t)2, p.empirical().size());
 	std::stringstream ss;
 	ss << p.empirical();
 	CHECK_EQUAL(0, ss.str().compare("H2.3 O"));
-	std::cerr << "TEST(ParserProcessingRegular3) stop" << std::endl;
 }
 
 #define TEST_FORMULA_4 "1H5 1H7"
 TEST(ParserProcessingRegular4)
 {
-	std::cerr << "TEST(ParserProcessingRegular4) start" << std::endl;
 	cfp::Parser p(TEST_FORMULA_4, sizeof(TEST_FORMULA_4)-1);
 	p.process();
 	CHECK_EQUAL((size_t)1, p.empirical().size());
 	std::stringstream ss;
 	ss << p.empirical();
 	CHECK_EQUAL(0, ss.str().compare("(1H)12"));
-	std::cerr << "TEST(ParserProcessingRegular4) stop" << std::endl;
 }
 
 #define TEST_FORMULA_5 "1H 5 H7"
 TEST(ParserProcessingRegular5)
 {
-	std::cerr << "TEST(ParserProcessingRegular5) start" << std::endl;
 	cfp::Parser p(TEST_FORMULA_5, sizeof(TEST_FORMULA_5)-1);
 	p.process();
 	CHECK_EQUAL((size_t)2, p.empirical().size());
 	std::stringstream ss;
 	ss << p.empirical();
 	CHECK_EQUAL(0, ss.str().compare("H7 (1H)5"));
-	std::cerr << "TEST(ParserProcessingRegular5) stop" << std::endl;
 }
 
 /* internal datastructures (std::map) don't preserve order (sort lexically instead)
@@ -150,7 +133,6 @@ TEST(ParserProcessingLoop)
 
 TEST(ParserProcessingComplex)
 {
-	std::cerr << "TEST(ParserProcessingComplex) start" << std::endl;
 	cfp::Parser p("K3(J(4)2(J4(K2.2(F3J))3F2.3))2.5",32);
 	p.process();
 	CHECK_EQUAL((size_t)4, p.empirical().size());
@@ -158,6 +140,5 @@ TEST(ParserProcessingComplex)
 	ss2 << p.empirical();
 	CHECK_EQUAL(0, ss2.str().compare("F28.25 J17.5 (4J)5 K19.5"));
 	CHECK_EQUAL(0, p.toMarkup().compare("K<sub>3</sub>(<sup>4</sup>J<sub>2</sub>(J<sub>4</sub>(K<sub>2.2</sub>(F<sub>3</sub>J))<sub>3</sub>F<sub>2.3</sub>))<sub>2.5</sub>"));
-	std::cerr << "TEST(ParserProcessingComplex) stop" << std::endl;
 }
 
